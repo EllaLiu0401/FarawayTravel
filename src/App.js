cc345ae263edd840fb3256d4428f796c87f0de90
EllaLiu0401 <ellatong0515@gmail.com>
2024-06-27 16:28:29 +1000

    Feat: Add Form search selection.
    - Use Array.from() and map() to get select option.
    - Prevent form default.
    
    Involved files:
    - src/App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 1, description: "Charger", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 12, packed: false },
 ];
 
 export default function App() {
@@ -19,10 +19,22 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 🥰 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..."></input>
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
@@ -30,7 +42,7 @@ function PackingList() {
     <div className="list">
       <ul className="list">
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
